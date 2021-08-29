f407b39c0be0447c196435c16475eb9a2778cb10
Harshit <bkharshitbk@gmail.com>
2021-08-29 17:32:18 +0530

    some basic routes

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// import express
 const express = require("express");
+
+// use express
 const app = express();
+
+// defining port
+const port = 8000;
+
+// main page
+app.get("/", (req, res) => {
+    return res.send("Home page");
+});
+
+// login
+app.get("/login", (req, res) => {
+    return res.send("This is Login Page!");
+});
+
+app.listen(port, () => {
+    console.log("Server is up and running");
+});
