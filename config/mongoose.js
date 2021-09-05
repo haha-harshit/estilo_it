d93d87eec081d92ad32d5fb0b838c5ab092ac966
Harshit <bkharshitbk@gmail.com>
2021-09-06 02:21:53 +0530

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useUnifiedTopology: true,
     // useCreateIndex: true,
 });
 
